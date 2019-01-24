a04869509debaae6f5d5bfd4c3bcf1109dfc816b
anusky95 <agarani95@gmail.com>
2019-01-23 20:25:58 -0500

    Definitions and facts about SQL and Relational Databases

---
+++ DescriptiveSQL/CRUDBasic.sql
@@ -0,0 +1,22 @@
+Creating table named Employee
+
+CREATE TABLE Employee (
+	EmployeeID int,
+	FirstName varchar(255),
+	LastName varchar(255),
+	Address varchar(255),
+	City varchar(255)
+	DepartmentID int
+
+);
+
+Select table -Displays all information
+
+SELECT *
+FROM Employee;
+
+Drop table - deletes a table
+
+DROP TABLE Employee;
+
+
